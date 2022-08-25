c8c439c544a34a20d4248210c0610b4eb4e8ded3
Sejla <calakovicsejla6@gmail.com>
2022-08-25 20:35:39 +0200

    :

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { useContext } from "react";
+import { Context } from "./UserContext";
+const App = () => {
+  const { user, setUser } = useContext(Context);
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setUser(user - 1);
+        }}
+      >
+        -
+      </button>
+      <h1>{user}</h1>
+      <button
+        onClick={() => {
+          setUser(user + 1);
+        }}
+      >
+        +
+      </button>
+    </div>
+  );
+};
+
+export default App;
