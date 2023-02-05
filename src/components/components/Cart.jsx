eeb5af90cd67b353ccd50e2ca1057050290f577e
Sejla <calakovicsejla6@gmail.com>
2023-02-05 21:46:28 +0100

    commit

--- src/components/components/Cart.jsx
+++ src/components/components/Cart.jsx
@@ -31,7 +31,7 @@ const Cart = (props) => {
     let total = 0;
 
     articles.forEach((article) => {
-      console.log("calculating total of articLES");
+      console.log("calculating total of articlES");
       total += article.price;
     });
 
